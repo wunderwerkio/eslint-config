c77b7ff7ab55a91660f2248361dc6c5dd8f3f678
Christian Foidl <christian@wunderwerk.io>
2023-09-07 17:45:25 +0200

    improve jsdoc

--- index.js
+++ index.js
@@ -1,53 +1,23 @@
 module.exports = {
-  plugins: ['import', 'jsdoc'],
-  extends: ['plugin:jsdoc/recommended', 'prettier'],
+  extends: ['./base.js', 'plugin:jsdoc/recommended'],
   rules: {
-    'import/first': 'warn',
-    'no-var': 'warn',
-    'no-empty': 'warn',
-    'no-unused-vars': [
-      'warn',
-      {
-        varsIgnorePattern: '^_',
-        argsIgnorePattern: '^_',
-      },
-    ],
-    'no-console': [
-      'warn',
-      {
-        allow: ['assert'],
-      },
-    ],
-    'no-fallthrough': 'warn',
-    'no-case-declarations': 'error',
-    'no-self-assign': 'warn',
-    'new-cap': 'warn',
-    'no-undefined': 'off',
-    'no-global-assign': 'error',
-    'prefer-const': 'error',
-    'dot-notation': 'error',
-    'no-void': 'error',
-    'no-useless-catch': 'error',
-    'no-prototype-builtins': 'off',
-    'jsdoc/require-jsdoc': [
-      'error',
-      {
-        publicOnly: false,
-        require: {
-          ClassDeclaration: true,
-          ClassExpression: true,
-          ArrowFunctionExpression: true,
-          MethodDefinition: true,
-        },
-      },
-    ],
+    // 'jsdoc/require-jsdoc': [
+    //   'error',
+    //   {
+    //     publicOnly: false,
+    //     require: {
+    //       ClassDeclaration: true,
+    //       ClassExpression: true,
+    //       ArrowFunctionExpression: true,
+    //       MethodDefinition: true,
+    //     },
+    //   },
+    // ],
     'jsdoc/require-description': [
       'error',
       {
         contexts: ['any'],
       },
     ],
-    'jsdoc/require-description-complete-sentence': 'warn',
-    'jsdoc/require-hyphen-before-param-description': 'warn',
   },
 };
--- typescript.js
+++ typescript.js
@@ -1,6 +1,6 @@
 module.exports = {
   plugins: ['@typescript-eslint'],
-  extends: ['./index.js', 'plugin:@typescript-eslint/recommended'],
+  extends: ['./base.js', 'plugin:jsdoc/recommended-typescript', 'plugin:@typescript-eslint/recommended'],
   overrides: [
     {
       files: ['*.ts', '*.tsx'],
@@ -20,15 +20,33 @@ module.exports = {
             objectLiteralTypeAssertions: 'never',
           },
         ],
-        'jsdoc/no-types': [
+        'jsdoc/require-returns': 'off',
+        'jsdoc/require-description-complete-sentence': 'warn',
+        'jsdoc/require-hyphen-before-param-description': 'warn',
+        'jsdoc/no-blank-block-descriptions': 'error',
+        'jsdoc/require-jsdoc': [
           'warn',
           {
-            contexts: ['any'],
+            publicOnly: false,
+            exemptEmptyFunctions: false,
+            exemptEmptyConstructors: false,
+            require: {
+              ArrowFunctionExpression: false,
+              ClassDeclaration: false,
+              ClassExpression: false,
+              FunctionDeclaration: false,
+              FunctionExpression: false,
+              MethodDefinition: false,
+            },
+            contexts: [
+              'ClassDeclaration',
+              'ClassExpression',
+              // Select a FunctionDeclaration that is not a descendant of
+              // another FunctionDeclaration.
+              ':function:not(:function :function)',
+            ],
           },
         ],
-        'jsdoc/require-param-type': 'off',
-        'jsdoc/require-returns': 'off',
-        'jsdoc/require-returns-type': 'off',
       },
     },
   ],
